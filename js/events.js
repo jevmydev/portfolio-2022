5e4a2bb770ec3ead6cad904a4f31db891837f0ef
Jeremy Díaz <jeremy664k@gmail.com>
2022-12-31 22:02:36 -0300

    Feature: new year and add title in timelines. Fix: events date

--- js/events.js
+++ js/events.js
@@ -1,15 +1,24 @@
-import { $ } from "./assets/selectors.js"
+import { $ } from "./assets/selectors.js";
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const actualDate = new Date();
 const actualDateString = actualDate.toDateString();
+const actualDayNumber = actualDate.getDay();
+
+const actualDay = days[actualDayNumber];
 const actualYear = actualDate.getFullYear();
 
 const $me = $(".aboutme__figure--image");
 
 const datesEvents = {
     christmas: {
-        dates: [`Sat Dec 24 ${actualYear}`, `Sun Dec 25 ${actualYear}`],
+        dates: [`${actualDay} Dec 24 ${actualYear}`, `${actualDay} Dec 25 ${actualYear}`],
         src: `../images/events/mechristmas.webp`
+    },
+    newYear: {
+        dates: [`${actualDay} Dec 31 ${actualYear}`, `${actualDay} Jan 01 ${actualYear}`],
+        src: `../images/events/meyear.webp`
     }
 }
 
